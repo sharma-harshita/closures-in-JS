a0916df76a422342b80ae409fa3f9958e8e992ff
harshita.sharma <harshita.sharma@asksid.ai>
2022-01-10 21:01:29 +0530

    changes

--- index.js
+++ index.js
@@ -17,14 +17,14 @@ let arr = [1,2,3,4,5];
 
 for (let i = 0; i < arr.length; i++) {
     setTimeout(()=>{
-        console.log(arr[i]);
+        console.log(arr[i], i);
     }, arr[i]*1000);
 }
 
 
 for (var i = 0; i < arr.length; i++) {
     setTimeout(()=>{
-        console.log(arr[i]);
+        console.log(arr[i], i , arr[0]);
     }, arr[i]*1000);
 }
 
